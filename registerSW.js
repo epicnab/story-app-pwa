2c42dbcd6bdec27a3efe5a342454193022e7ebfa
epicnab <a.nabilah.p1@gmail.com>
2025-12-22 10:41:08 +0700

    Fix asset paths for GitHub Pages deployment

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/story-app-pwa/sw.js', { scope: '/story-app-pwa/' })})}

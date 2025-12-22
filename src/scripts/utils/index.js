fc395823e1ab0409ffc77f7bc173c8a97d888e95
epicnab <a.nabilah.p1@gmail.com>
2025-12-22 10:35:09 +0700

    Deploy built application to GitHub Pages

--- src/scripts/utils/index.js
+++
@@ -1,8 +0,0 @@
-export function showFormattedDate(date, locale = 'en-US', options = {}) {
-  return new Date(date).toLocaleDateString(locale, {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    ...options,
-  });
-}
--- src/scripts/utils/map.js
+++
@@ -1,7 +0,0 @@
-export function getLeaflet() {
-  if (window.L) {
-    return window.L;
-  }
-
-  throw new Error('Leaflet is not loaded. Please ensure the CDN script is included in the HTML.');
-}

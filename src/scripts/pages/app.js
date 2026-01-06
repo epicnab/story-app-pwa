e74951801502f3b18e9097a833dc42c758426831
epicnab <a.nabilah.p1@gmail.com>
2026-01-06 18:24:46 +0700

    push notification and label fix

--- src/scripts/pages/app.js
+++ src/scripts/pages/app.js
@@ -1,6 +1,17 @@
 import routes from '../routes/routes';
 import { getActiveRoute } from '../routes/url-parser';
 
+const pageTitles = {
+  "/": "Home | Story App",
+  "/about": "About Us | Story App",
+  "/login": "Login | Story App",
+  "/register": "Register | Story App",
+  "/stories": "All Stories | Story App",
+  "/add-story": "Add a New Story | Story App",
+  "/story/:id": "Story Details | Story App",
+  "/offline": "Offline Stories | Story App",
+};
+
 class App {
   #content = null;
   #drawerButton = null;
@@ -38,6 +49,8 @@ class App {
   async renderPage() {
     const url = getActiveRoute();
     const page = routes[url];
+
+    document.title = pageTitles[url] || "Story App";
 
     if ('startViewTransition' in document) {
       await document.startViewTransition(async () => {
